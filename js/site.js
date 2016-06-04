0677bbbe7c87089248dc318a3198b8a25d3012c7
Justin Svegliato <justin.svegliato1@marist.edu>
2016-06-04 09:15:53 -0400

    Polished up the site (email, mobile fixes, and s'more)

--- js/site.js
+++ js/site.js
@@ -1,26 +1,26 @@
 $(function() {
   var CONTAINER_ELEMENT = '.mdl-layout__content';
   var IDS = ['#banner', '#about', '#education', '#research', '#industry', '#honors'];
-    
-  $('a').click(function(){
-	$(CONTAINER_ELEMENT).animate({
-	  scrollTop: $(CONTAINER_ELEMENT).scrollTop() + $(this.hash).offset().top - 55
-	}, 1000, 'swing');
-	return false;
-  });
 
   var offsets = {};
   IDS.forEach(function(id) {
-	offsets[id] = $(id).offset().top;
+    offsets[id] = $(id).offset().top;
+  });
+
+  $('a').click(function(){
+    $(CONTAINER_ELEMENT).animate({
+      scrollTop: $(CONTAINER_ELEMENT).scrollTop() + $(this.hash).offset().top - 55
+    }, 1000, 'swing');
+    return false;
   });
 
   $(CONTAINER_ELEMENT).on('scroll', function() {
-	IDS.forEach(function(id) {
-	  var offset = offsets[id];
-	  if ($(CONTAINER_ELEMENT).scrollTop() + 80 >= offset) {
-		$('a').removeClass('active');
-		$('a[href*=' + '"' + id + '"]').addClass('active');			      	
-	  }
-	});		    
+    IDS.forEach(function(id) {
+      var offset = offsets[id];
+      if ($(CONTAINER_ELEMENT).scrollTop() + 80 >= offset) {
+        $('a').removeClass('active');
+        $('a[href*=' + '"' + id + '"]').addClass('active');
+      }
+    });
   });
 });
